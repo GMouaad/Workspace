aac6d1aa4d05ed132d77dc767475b69d4ea39bbe
GMouaad <Mgsair@gmail.com>
2016-12-03 14:25:32 +0100

    new functions.c file added // new prototypes.h header file added // added some comments on the use of the funtions

---
+++ C_Mini_Projekt/Prototypes.h
@@ -0,0 +1,19 @@
+/*
+ * Prototypes.h
+ *
+ *  Created on: Dec 3, 2016
+ *      Author: MouaadGssair
+ */
+
+#ifndef PROTOTYPES_H_
+#define PROTOTYPES_H_
+
+
+void printline(int count);
+void display();
+void swap(int num1, int num2);
+int partition(int left, int right, int pivot);
+void quickSort(int left, int right);
+
+
+#endif /* PROTOTYPES_H_ */
--- C_Mini_Projekt/main.c
+++ C_Mini_Projekt/main.c
@@ -5,4 +5,21 @@
  *      Author: MouaadGssair
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX 7
+
+int intArray[MAX] = {4,6,3,2,1,9,7};
+
+int main(int argc, char **argv)
+{
+	printf("Input Array: ");
+	   display();
+	   printline(50);
+	   quickSort(0,MAX-1);
+	   printf("Output Array: ");
+	   display();
+	   printline(50);
+}
